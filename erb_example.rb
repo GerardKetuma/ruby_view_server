f73244bb6bc083c3db8b5c602dd62823dc16f62f
Gerard Ketuma <gketuma@hotmail.com>
2013-04-17 02:25:15 -0500

    Add new lines of code to the erb example file to display an unordered list of food items

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,14 @@
 require 'erb'
 
 x = 42
+arry = ["Beef", "Corn", "Tomatoes"]
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+template2 = ERB.new '
+<ul>
+<% arry.each do |el| %>
+  <li><%= "I love #{el}" %></li>
+<% end %>
+</ul>
+'
+puts template2.result(binding)
